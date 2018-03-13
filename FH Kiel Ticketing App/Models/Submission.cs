6408da56c0b99ed6bb4ba45cddc88b8de42a6222
Hussainih <hussaini.mohd86@gmail.com>
2018-03-13 23:26:57 +0100

    Project Submission

---
+++ FH Kiel Ticketing App/Models/Submission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FH_Kiel_Ticketing_App.Models
+{
+    public class Submission
+    {
+        [Key]
+        public int RecID { get; set; }
+        public virtual User User { get; set; }
+        public virtual Ticket Ticket { get; set; }
+        public virtual ICollection<Files> Files{get; set;}
+        public DateTime submissionDate { get; set; }
+    }
+}
